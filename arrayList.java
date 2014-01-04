c922906a672350c6e24df232a34498bab0c1242c
Nino Ninux <nino.ninux@gmail.com>
2014-01-04 10:55:42 +0100

    Methoden zu Map<k,v> hinzugefügt

--- arrayList.java
+++ arrayList.java
@@ -11,10 +11,24 @@ public class ArrayListAndIterator{
 		words.add("Bye");
 	}
 
+	// Iterieren mit Iterator
 	public void printWords1(){
 		Iterator<String> itr = words.iterator();
-
 		while(itr.hasNext()){
 			System.out.println(itr.next());
 		}
 	}
+
+	// Iterieren mit for-loop
+	public void printWords2(){
+		for(int i = 0, n = words.size(); i < n; i++){
+			System.out.println(words.get(i));
+		}
+	}
+
+	// Iterieren mit for-each
+	public void printWords3(){
+		for(String s : words){
+			System.out.println(s);
+	}
+}
